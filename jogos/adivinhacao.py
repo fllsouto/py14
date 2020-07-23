76cc7298fd27c67bc9886a9706fcc0eadaaf251d
Fellipe Souto <fllsouto@gmail.com>
2020-07-22 22:52:42 -0300

    exercício 3.14

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,17 +4,26 @@ print('****************************************************')
 
 numero_secreto = 42
 
-chute = int(input('Digite o seu chute: '))
-print('Você digitou:', chute)
+
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+  print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
+  chute = int(input('Digite o seu chute: '))
+  print('Você digitou:', chute)
 
 
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+  acertou = numero_secreto == chute
+  maior = chute > numero_secreto
+  menor = chute < numero_secreto
 
-if (acertou):
-  print('Você acertou')
-elif (maior):
-  print('Você errou! Seu chute foi maior que o número secreto')
-elif (menor):
-  print('Você errou! Seu chute foi menor que o número secreto')
+  if (acertou):
+    print('Você acertou')
+    break
+  elif (maior):
+    print('Você errou! Seu chute foi maior que o número secreto')
+  elif (menor):
+    print('Você errou! Seu chute foi menor que o número secreto')
+  
+  rodada += 1
